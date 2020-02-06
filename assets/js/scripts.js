c0f26e84fb9ddce165abf846af3e37c382f3f5cb
Arhell <arhell333@gmail.com>
2020-02-07 00:06:56 +0200

    rename var

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -158,17 +158,17 @@ $(document).ready(
       $('.responsive_nav').click(
         function () {
             var nav_element;
-            var body_element;
+            var html_element;
 
             nav_element  = $('nav');
-            body_element = $('html');
+            html_element = $('html');
 
             if (nav_element.hasClass('slide')) {
                 nav_element.removeClass('slide');
-                body_element.removeClass('overflow');
+                html_element.removeClass('overflow');
             } else {
                 nav_element.addClass('slide');
-                body_element.addClass('overflow');
+                html_element.addClass('overflow');
             }
         }
       );
